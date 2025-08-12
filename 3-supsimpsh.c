1a0ba1cf958667d75f76b789d519a63f2dcce0eb
harmel3056 <melh1988@hotmail.com>
2025-08-12 21:24:46 +0000

    Annotation on working file for super simple shell

--- 3-supsimpsh.c
+++ 3-supsimpsh.c
@@ -9,23 +9,23 @@
 
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	char *token;
+	char *line = NULL;	//Feeds getline buffer, NULL to ensure malloc
+	size_t len = 0;		//Holds allocated size of getline buffer
+	char *token;		//Hosts strtok, ret pointer to next token
 
 	printf("$ ");
-	getline(&line, &len, stdin);
+	getline(&line, &len, stdin);	//line now points to buffer containing line read from stdin, len holds malloc'd buffer size
 
-	line[strcspn(line, "\n")] = '\0';
+	line[strcspn(line, "\n")] = '\0';	//replaces '\n' with '\0' to remove newline appld by getline
 
-	token = strtok(line, ",");
+	token = strtok(line, ",");	//initialises tokenising with strtok
 
-	while (token != NULL)
+	while (token != NULL)		//token/pointer is valid
 	{
-		printf("%s\n", token);
-		token = strtok(NULL, ",");
+		printf("%s\n", token);		//print token segment
+		token = strtok(NULL, ",");	//tokenise following segments of string, NULL to continue same string, strtok remembers where you're up to
 	}
 
-	free(line);
+	free(line);		//release malloc
 	return (0);
 }
