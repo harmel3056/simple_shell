7bc5dfdec04e0bfa596a92e6aef0e021a5b57df5
harmel3056 <melh1988@hotmail.com>
2025-08-15 01:05:33 +0000

    _getenv dupe

--- e2-_getenv.c
+++ e2-_getenv.c
@@ -1,10 +1,61 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 extern char **environ;
 
 char *_getenv(const char *name)
 {
+	char *var;
+	char *val; 
+	char *env_copy; //make copy of environ to prevent strtok modifying the environ itself
+	char *result;
+	int i = 0;
 
+	while (environ[i] != NULL)
+	{ 
+		env_copy = strdup(environ[i]);
+
+		if (env_copy == NULL)
+			return (NULL);
+
+		var = strtok(env_copy, "=");
+		val = strtok(NULL, "=");
+
+		if (strcmp(var, name) == 0)
+		{
+			if (val != NULL)
+			       result = strdup(val);
+			else
+				result = NULL;
+
+			free(env_copy);
+			return (result);
+		}
+	
+	free(env_copy);
+	i++;
+	}
+	
+	return (NULL);
+}
+
+int main(void)
+{
+	char *path_val;
+
+	path_val = _getenv("PATH");
+
+	if (path_val != NULL)
+	{
+		printf("PATH = %s\n", path_val);
+		free(path_val);
+	}
+
+	else
+		printf("nah mate\n");
+
+	return (0);
 }
 
 //Use strtok to tokenise the environment lists' array items
